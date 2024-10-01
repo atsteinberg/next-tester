8d765407b5f64ef48adf1c982e961b14a008d979
Alex Steinberg <alexander.steinberg@hoval.com>
2024-10-01 16:54:09 +0200

    update stuff

--- packages/next/src/app/page.tsx
+++ packages/next/src/app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
           height={38}
           priority
         />
+        <p>Hey you!</p>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
@@ -37,7 +38,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now packages
+            Deploy now
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
